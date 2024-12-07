9d81d824df7b884b7acbf68f2554557e084cc64a
Evyyl <85968488+Evyyl@users.noreply.github.com>
2024-12-07 18:49:51 -0300

    entidade

--- src/dominios/avaliacao.cpp
+++ src/dominios/avaliacao.cpp
@@ -1,12 +1,14 @@
-#include "../../include/dominios/avaliacao.h"
+#include "../../include/dominios/avaliacao.hpp"
 
 /**
  * @brief Método para validar o valor da avaliação.
  * @param valor Valor a ser validado.
  * @throw std::invalid_argument Se o valor não estiver entre 1 e 5.
  */
-void Avaliacao::validar(int valor) {
-    if (valor < 1 || valor > 5) {
+void Avaliacao::validar(int valor)
+{
+    if (valor < 1 || valor > 5)
+    {
         throw std::invalid_argument("Avaliacao invalida. Deve estar entre 1 e 5.");
     }
 }
@@ -15,7 +17,8 @@ void Avaliacao::validar(int valor) {
  * @brief Construtor da classe Avaliacao.
  * @param valor Avaliação inicial.
  */
-Avaliacao::Avaliacao(int valor) {
+Avaliacao::Avaliacao(int valor)
+{
     validar(valor);
     this->valor = valor;
 }
@@ -24,7 +27,8 @@ Avaliacao::Avaliacao(int valor) {
  * @brief Define o valor da avaliação.
  * @param valor Nova avaliação.
  */
-void Avaliacao::setValor(int valor) {
+void Avaliacao::setValor(int valor)
+{
     validar(valor);
     this->valor = valor;
 }
@@ -33,6 +37,7 @@ void Avaliacao::setValor(int valor) {
  * @brief Obtém o valor da avaliação.
  * @return Valor da avaliação.
  */
-int Avaliacao::getValor() const {
+int Avaliacao::getValor() const
+{
     return valor;
 }
